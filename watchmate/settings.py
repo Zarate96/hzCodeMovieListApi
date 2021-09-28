8d29d5074e34d527d7c2e7f20edd9cc52264bb4a
Your Name <you@example.com>
2021-09-28 13:45:31 -0500

    config apps

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #My_apps
+    'watchlist_app',
+
+    #Third_apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
